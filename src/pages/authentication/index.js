6f8f4dedc4ea8cb142fb535f27f8ad94608e462d
sergey <siteabouteat@gmail.com>
2020-06-13 13:58:58 +0300

    second commit

--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -1,15 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Axios from 'axios'
 
 export const Authentication = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
   const handleSubmit = e => {
     e.preventDefault()
+    setIsSubmitting(true)
     console.log('Email:Password', email, password)
   }
+
+  const sleepIsSubmitting = () => {
+    setTimeout(() => {
+      setIsSubmitting(false)
+    }, 3000)
+  }
+
+  useEffect(() => {
+    if(!isSubmitting) return;
+    Axios('https://conduit.productionready.io/api/users', {
+      method: 'post',
+      data: {
+        user: {
+          email: 'ssdd',
+          password: '123212'
+        }
+      }
+    }).then(res => {
+      console.log('Success', res)
+      sleepIsSubmitting()
+    }).catch(err => {
+      console.error('Error', err)
+      sleepIsSubmitting()
+    })
+  }, [isSubmitting])
 
   return (
     <div className="auth-page">
@@ -41,6 +69,7 @@ export const Authentication = () => {
                   />
                 </fieldset>
                 <button
+                  disabled={ isSubmitting }
                   className="btn btn-lg btn-primary pull-xs-right"
                   type="submit"
                 >
