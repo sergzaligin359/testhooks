a4324b14c7b950817f5e59b91ddb9444c1f52a77
sergey <siteabouteat@gmail.com>
2020-06-16 11:39:14 +0300

    fix

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {Link} from 'react-router-dom'
 
-export const Article = () => {
-    return (
-        <div>Article page</div>
-    )
+import { useFetch } from '@/hooks'
+import { Loading, ErrorMessage, TagList } from '@/components'
+
+export const Article = props => {
+  const slug = props.match.params.slug
+  const apiUrl = `/articles/${slug}`
+  const [{response, error, isLoading}, doFetch] = useFetch(apiUrl)
+
+  useEffect(() => {
+    doFetch()
+  }, [doFetch])
+
+  return (
+    <div className="article-page">
+      <div className="banner">
+        {!isLoading && response && (
+          <div className="container">
+            <h1>{response.article.title}</h1>
+            <div className="article-meta">
+              <Link to={`/profiles/${response.article.author.username}`}>
+                <img src={response.article.author.image} alt="" />
+              </Link>
+              <div className="info">
+                <Link to={`/profiles/${response.article.author.username}`}>
+                  {response.article.author.username}
+                </Link>
+                <span className="date">{response.article.createdAt}</span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="container page">
+        {isLoading && <Loading />}
+        {error && <ErrorMessage />}
+        {!isLoading && response && (
+          <div className="row article-content">
+            <div>
+              <p>{response.article.body}</p>
+            </div>
+            <TagList tags={response.article.tagList} />
+          </div>
+        )}
+        <hr />
+      </div>
+    </div>
+  )
 }
+
+export default Article
